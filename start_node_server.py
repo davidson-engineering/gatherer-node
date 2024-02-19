a440a44086c7e45ec26258950326aeb74e0a635c
Matthew Davidson <matthew@davidson.engineering>
2024-02-19 13:55:17 -0800

    added logging

--- start_node_server.py
+++ start_node_server.py
@@ -1,28 +1,35 @@
-import asyncio
-import logging
-import os
+
+
+from logging.config import dictConfig
 from dotenv import load_dotenv
-
-load_dotenv()
-
-HOSTNAME = os.getenv('HOSTNAME')
-PORT = int(os.getenv('PORT'))
+import os
 
 from data_node_network.node_data_gatherer import GathererNodeTCP
 
-logger = logging.getLogger(__name__)
+load_dotenv()
+HOSTNAME = os.getenv('HOSTNAME')
+PORT = int(os.getenv('PORT'))
 
+def setup_logging(filepath="config/logger.yaml"):
+    import yaml
+    from pathlib import Path
+
+    if Path(filepath).exists():
+        with open(filepath, "r") as stream:
+            config = yaml.load(stream, Loader=yaml.FullLoader)
+    else:
+        raise FileNotFoundError
+    logger = dictConfig(config)
+    return logger
 
 def main():
     address = (HOSTNAME, PORT)
-    node_id = 0
 
     # Create and start the server
     node_server = GathererNodeTCP(address=address)
     node_server.start()
 
-
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    # asyncio.run(main())
+    
+    setup_logging()
     main()
